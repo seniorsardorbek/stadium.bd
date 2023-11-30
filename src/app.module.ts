e6c1d85319fb8f13227f783149cae975a36c866e
seniorsardorbek <sardorbekmusilman@gmail.com>
2023-11-30 10:43:18 +0500

    path

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, RequestMethod } from "@nestjs/common";
+import {  Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { MongooseModule } from "@nestjs/mongoose";
@@ -11,11 +11,8 @@ import { ServeStaticModule } from "@nestjs/serve-static";
 import { join } from "path";
 import { CommentsModule } from "./comments/comments.module";
 import { BookingsModule } from "./bookings/bookings.module";
-import { MailerModule } from "@nestjs-modules/mailer";
-import {} from "@nestjs/platform-express";
 import { MailModule } from "./mail/mail.module";
 import { EventsModule } from "./events/events.module";
-import { TwilioModule } from "nestjs-twilio";
 import { OwnersModule } from './owners/owners.module';
 import { StatisticsModule } from './statistics/statistics.module';
 @Module({
@@ -30,10 +27,6 @@ import { StatisticsModule } from './statistics/statistics.module';
     }),
     MailModule,
     PassportModule.register({ session: true }),
-    TwilioModule.forRoot({
-      accountSid: "ACb7080c3956547034e471cbd1887b3a60",
-      authToken: "eaccd61cffe1c89417d77405ca416df1",
-    }),
     StadionsModule,
     UserModule,
     AuthModule,
